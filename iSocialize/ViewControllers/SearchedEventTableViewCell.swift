51cafd5106b615062f00a00bf458d6178af25ab8
Jovan R <jovanr@jovans-macbook-pro.local>
2018-11-15 11:16:32 -0500

    Infrastructure Commit

---
+++ iSocialize/ViewControllers/SearchedEventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchedEventTableViewCell.swift
+//  iSocialize
+//
+//  Created by Jovan R on 11/15/18.
+//  Copyright © 2018 Jovan Rivera. All rights reserved.
+//
+
+import UIKit
+
+class SearchedEventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
